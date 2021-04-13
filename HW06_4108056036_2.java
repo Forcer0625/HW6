21200c30222e89da941b69c9559bf9a0df7e75ca
Forcer0625 <elison8934678@gmail.com>
2021-04-13 18:00:03 +0800

    mini optimized

--- HW06_4108056036_2.java
+++ HW06_4108056036_2.java
@@ -1,47 +1,44 @@
 public class HW06_4108056036_2 extends Dessert_Desert
 {
-    static int arr_len,len;
-    static int[] A,ans,max=new int[100000],min=new int[100000];
-    @Override
-    public int[] maxBlocks(int[][] arr)
-    {
-        arr_len=arr.length;
-        ans=new int[arr_len];
-        for(int i=arr_len-1;i>=0;i--)
-        {
-            A=arr[i];
-            len=A.length;
-            int count=0;
-            max[0]=min[0]=A[0];
-            for(int j=1;j<len;j++)
-            {
-                if(A[j]>=max[count])
-                {
-                    count++;
-                    max[count]=min[count]=A[j];
-                }
-                else if(A[j]<min[count])
-                {
-                    min[count]=A[j];
-                    for (int check=count-1;check>=0;check--)
-                    {
-                        if (A[j]>=max[check]) break;
-                        else if (A[j]>=min[check])
-                        {
-                            count--;
-                            max[check]=max[check+1];
-                            break;
-                        }else
-                        {
-                            count--;
-                            min[check]=A[j];
-                            max[check]=max[check+1];
-                        }
-                    }
-                }
-            }
-            ans[i]=++count;
-        }
-        return ans;
-    }
+	static int len,arr_len;
+	final static int[] min = new int[100000];
+	final static int[] max = new int[100000];
+	static int[] ans;
+	static int[] A;
+	@Override
+	public int[] maxBlocks(int[][] inputArr)
+	{
+		ans = new int[inputArr.length];
+		for(int i=inputArr.length-1;i>=0;i--)
+		{
+			A=inputArr[i];
+			int check=0;
+			int count=0;
+			min[check] = max[check] = A[0];
+			len=A.length;
+			for(int j=1;j<len;j++)
+			{
+				if(min[check] > A[j])
+					min[check] = A[j];
+				else if(max[check] <= A[j])
+				{
+					check++;
+					min[check] = max[check] = A[j];
+				}
+			}
+			
+			for(int cur_min = min[check--];check>=0;check--)
+			{
+				if(cur_min >= max[check])
+				{
+					count++;
+					cur_min = min[check];
+				}
+				else if(cur_min > min[check])
+					cur_min = min[check];
+			}
+			ans[i]=++count;
+		}
+		return ans;
+	}
 }
